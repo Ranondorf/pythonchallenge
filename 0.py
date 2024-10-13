70eacbaa1c235aff8826f6b8f52e5a6aab371f8f
Rui <rui7@tuta.io>
2024-10-13 13:04:08 +0900

    Final answer

---
+++ 0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+
+class Solution:
+    def __init__(self):
+        pass
+    def run(self):
+        print(f"http://www.pythonchallenge.com/pc/def/{2**38}.html")
+
+
+
+
+
+answer = Solution()
+answer.run()
